49f90958fe5bf7d4b13ba1bbdb64004c5473ce62
Nicholas Zuber <zuber.nicholas@gmail.com>
2016-10-14 20:34:20 -0400

    fixed header file

--- src/bit_vector.h
+++ src/bit_vector.h
@@ -18,53 +18,80 @@ public:
     BitVector (void) {
         this->internal_size = 1;
         this->internal_array = new data_type[this->internal_size];
-        this->total_count = 0;
     };
     
-    ~BitVector (void) {};
+		/**
+		 * Destructor for BitVector.
+		 * @param  {void}
+		 * @return {void)
+		 */
+		~BitVector (void) {
+			delete [] this->internal_array;
+		}
     
     /**
-     * Inserts a single element into the BitVector at the given position.
+     * Attempts to set a particular bit within the BitVector.
      * @param  {const size_t&}    pos  The position within the vector.
-     * @param  {const data_type&} data The data to insert.
-     * @return {bool}                  The success of the insertion.
+     * @param  {const data_type&} data The bit to set.
+     * @return {bool}                  The success of the set.
      */
-    bool insert (const size_t& pos, const data_type& data);
+    bool set (const size_t& pos, const data_type& data);
     
     /**
-     * Removes a single element into the BitVector.
-     * @param  {const data_type&} data The data to remove.
-     * @return {bool}                  The success of the removal.
-     */
-    bool remove (const data_type& data);
-    
-    /**
-     * Syntactic sugar for the `at` method -- finds the element at the given position.
+     * Syntactic sugar for the `get` method -- provides the value of bit at the given position.
      * @param  {const size_t&} pos The position within the vector.
      * @return {bool}              The success of the look up.
      */
     data_type operator[] (const size_t& pos) const;
     
     /**
-     * Finds the element at the given position.
+		 * Provides the value of bit at the given position.
      * @param  {const size_t&} pos The position within the vector.
      * @return {bool}              The success of the look up.
      */
-    data_type at (const size_t& pos) const;
+    data_type get (const size_t& pos) const;
     
-    /**
-     * Provides the total amount of elements within the BitVector.
+		/**
+		 * Takes the complement of the calling BitVector.
+		 * @param  {void}
+		 * @return {const BitVector&} The complement BitVector.
+		 */
+		BitVector& complement (const BitVector& vec) const;
+
+		/**
+		 * Takes the union of the argument BitVector.
+		 * @param  {const BitVector&}
+		 * @return {const BitVector&} The complement BitVector.
+		 */
+		BitVector& union (const BitVector& vec) const;
+
+		/**
+		 * Takes the union of the calling BitVector.
+		 * @param  {const BitVector&}
+		 * @return {const BitVector&} The complement BitVector.
+		 */
+		BitVector& union () const;
+
+		/**
+		 * Takes the union of the argument BitVector.
+		 * @param  {const BitVector&}
+		 * @return {const BitVector&} The complement BitVector.
+		 */
+		BitVector& intersection (const BitVector& vec) const;
+
+		/**
+		 * Takes the union of the calling BitVector.
+		 * @param  {const BitVector&}
+		 * @return {const BitVector&} The complement BitVector.
+		 */
+		BitVector& intersection () const;
+
+		/**
+     * Provides the amount of bits allocated for the BitVector.
      * @param  {void}
-     * @return {size_t} The total amount of elements.
+     * @return {size_t} The total amount of bits.
      */
     size_t size () const;
-    
-    /**
-     * Provides the total amount of allocated space within the BitVector.
-     * @param  {void}
-     * @return {size_t} The total amount of allocated space.
-     */
-    size_t capacity () const;
     
     /**
      * Changes the capacity of the BitVector.
@@ -79,10 +106,7 @@ private:
     
     /** Pointer to the internal array. */
     data_type* internal_array;
-    
-    /** Total amount of elements in the array. */
-    size_t total_count;
-    
 };
 
 #endif /* INCLUDE_BIT_VECTOR */
+ 
